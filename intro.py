d870e8864dabb0d2c6776f9f4ea12f70cc301ddf
Luke <rio8562@icloud.com>
2022-05-28 22:09:16 -0400

    going through the fundamentals of python again

---
+++ intro.py
@@ -0,0 +1,36 @@
+def main():
+    # you'll need comments to explain what's going on
+    print("hello")
+    print(4)
+
+    print(type(5))
+    print(type("hello"))
+    print(type(True))
+
+    print(type(2.1))
+
+    """ hello 
+    this is a multiline
+    comment """
+
+    # the end="" lets you have the line below it on the same line
+    print("hello", 5, end="")
+    print("lucas")
+
+    # what are variables?
+    # x + 2 = 3, the x is a variable in math
+
+    x = 3
+    print(x)
+
+    num = x
+    print(num)
+
+    name = "Lucas"
+
+    max_score = 10
+
+    # classes start with an uppercase letter
+
+
+main()
